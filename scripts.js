f8339773c1644b903aa6e46ce5ff244f2f780a39
Matthew Thomas <adriant172@gmail.com>
2022-05-06 23:25:21 +0900

    Remove y axis and change to just one container div

--- scripts.js
+++ scripts.js
@@ -1,5 +1,13 @@
 const xAxis = document.querySelector("#x-axis");
+const yAxis = document.querySelector("#y-axis");
 
 for (let i = 0; i < 16; i++) {
-    
-}
+    const xBox = document.createElement('div');
+    xBox.classList.add('xBox');
+    xAxis.appendChild(xBox);
+    for (let j = 0; j < 16; j++) {
+        const box = document.createElement('div');
+        box.classList.add('box');
+        xBox.appendChild(box);
+    };
+};
