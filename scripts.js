fb93f702b1535a8bc92e6ebdd0aa9ebaa0d85af8
Matthew Thomas <adriant172@gmail.com>
2022-05-08 22:46:34 +0900

    Add create grid function and corresponding CSS

--- scripts.js
+++ scripts.js
@@ -1,13 +1,22 @@
-const xAxis = document.querySelector("#x-axis");
-const yAxis = document.querySelector("#y-axis");
+const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
-    const xBox = document.createElement('div');
-    xBox.classList.add('xBox');
-    xAxis.appendChild(xBox);
-    for (let j = 0; j < 16; j++) {
+function createGrid (gridNum) {
+    const totalBoxes = gridNum * gridNum;
+    for (let i = 0; i < totalBoxes; i++) {
         const box = document.createElement('div');
         box.classList.add('box');
-        xBox.appendChild(box);
-    };
-};
+        container.appendChild(box);
+    }
+}
+
+createGrid(24)
+
+
+const boxes = document.querySelectorAll('.box');
+
+boxes.forEach((box) => {
+    box.addEventListener('mouseover',() =>{
+        box.classList.add('drawing');
+    });
+})
+
