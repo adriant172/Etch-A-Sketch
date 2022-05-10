a3f42ba09533ed6879192abcc1c1a8e0c0d2d73f
Matthew Thomas <adriant172@gmail.com>
2022-05-10 22:30:28 +0900

    Moved event listeners into the create grid function itself

--- scripts.js
+++ scripts.js
@@ -7,16 +7,45 @@ function createGrid (gridNum) {
         box.classList.add('box');
         container.appendChild(box);
     }
+    // Set up hover effect so grid boxes change color upon mouseover
+    let boxes = document.querySelectorAll('.box');
+    boxes.forEach((box) => {
+        box.addEventListener('mouseover',() =>{
+            box.classList.add('drawing');
+        });
+    })
 }
 
-createGrid(24)
 
-const boxes = document.querySelectorAll('.box');
 
-boxes.forEach((box) => {
-    box.addEventListener('mouseover',() =>{
-        box.classList.add('drawing');
-    });
+createGrid(16)
+
+// Set up hover effect so grid boxes change color upon mouseover
+
+const newGridbtn = document.querySelector('.newGrid');
+let newGridNum;
+
+
+
+newGridbtn.addEventListener('click', () => {
+
+    do {
+        newGridNum = prompt("How many squares per side would you like on your new grid ?");
+        newGridNum = parseInt(newGridNum);
+        if ( newGridNum >= 1 && newGridNum <= 100){
+            break;
+        }
+    } 
+    while (true);
+
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+    
+    container.style.gridTemplateColumns = `repeat(${newGridNum}, minmax(0, 1fr))`;
+    container.style.gridTemplateRows = `repeat(${newGridNum}, minmax(0, 1fr))`;
+    createGrid(newGridNum);
+    boxes = document.querySelectorAll('.box');
 })
 
 
